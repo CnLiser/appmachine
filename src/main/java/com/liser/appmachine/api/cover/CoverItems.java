2eb65db89dec68cf4d277c288956f7a3ab923338
79389 <793891972@qq.com>
2025-09-05 10:46:00 +0800

    me input bus cover 描述调整

--- src/main/java/com/liser/appmachine/api/cover/CoverItems.java
+++ src/main/java/com/liser/appmachine/api/cover/CoverItems.java
@@ -21,7 +21,7 @@ public class CoverItems {
             .onRegister(attach(new CoverPlaceBehavior(Covers.ME_INPUT_BUS_COVER)))
             .onRegister(attach(new TooltipBehavior(lines -> {
                 lines.add(Component.translatable("item.appmachine.me_input_bus_cover.tooltip"));
-                lines.add(Component.translatable("item.appmachine.me_input_bus_cover.tooltip.item_transfer_rate", 64));
+                lines.add(Component.translatable("item.appmachine.me_input_bus_cover.tooltip.item_transfer_rate"));
             })))
             .register();
 
