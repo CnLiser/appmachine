8d8253ad2f87aa3bbf3a238ef1e09da34da9d9e4
79389 <793891972@qq.com>
2025-09-24 19:02:58 +0800

    插槽功能实现，但是在重启后无法恢复选项卡，需要点击tab栏打开 在机器配置gui界面打开me_network_cover的配置界面，移动插槽时会触发奇怪的bug，暂时未查明原因，可能是nbt数据的问题，但是不会影响使用

--- src/main/java/com/liser/appmachine/api/cover/MENetworkCover.java
+++ src/main/java/com/liser/appmachine/api/cover/MENetworkCover.java
@@ -10,13 +10,14 @@ import com.liser.appmachine.api.cover.slot.CoverSlot;
 import com.liser.appmachine.api.cover.slot.CoverSlotHandler;
 import com.liser.appmachine.api.cover.slot.CoverSlotHandlers;
 import com.liser.appmachine.api.cover.trait.MECover;
-import com.lowdragmc.lowdraglib.gui.widget.LabelWidget;
-import com.lowdragmc.lowdraglib.gui.widget.Widget;
-import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
+import com.liser.appmachine.api.gui.widget.TabsWidget;
+import com.lowdragmc.lowdraglib.gui.texture.ColorRectTexture;
+import com.lowdragmc.lowdraglib.gui.widget.*;
 import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 import com.lowdragmc.lowdraglib.utils.Position;
+import com.lowdragmc.lowdraglib.utils.Size;
 import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.MethodsReturnNonnullByDefault;
@@ -28,8 +29,7 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.beans.PropertyChangeSupport;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
@@ -50,17 +50,22 @@ public class MENetworkCover extends MECover implements IUICover {
     @Getter
     private final CoverSlotHandler[] coverSlotHandler = new CoverSlotHandler[CONFIG_SIZE];
 
-
+    protected TabsWidget tabBar;
+    @Persisted
+    protected int tabBarIndex = 0;
+    protected boolean isLoad = false;
 
     public MENetworkCover(CoverDefinition definition, ICoverable coverHolder, Direction attachedSide) {
         super(definition, coverHolder, attachedSide);
         for (int i = 0; i < CONFIG_SIZE; i++) {
+            int index = i;
             CoverSlotHandler<ItemStack, CoverSlot> handler = CoverSlotHandlers.slot(this)
-                    .onSlotLoaded(f -> configureSlot(f, CoverSlotHandler.LOADED_ID))
-                    .onSlotRemoved(f -> configureSlot(f, CoverSlotHandler.REMOVE_ID))
-                    .onSlotUpdated(f -> configureSlot(f, CoverSlotHandler.UPDATE_ID));
+                    .onSlotLoaded(f -> configureSlot(f, CoverSlotHandler.LOADED_ID, index))
+                    .onSlotRemoved(f -> configureSlot(f, CoverSlotHandler.REMOVE_ID, index))
+                    .onSlotUpdated(f -> configureSlot(f, CoverSlotHandler.UPDATE_ID, index));
             coverSlotHandler[i] = (handler);
         }
+        creteWidgetGroup();
     }
 
     @Override
@@ -74,23 +79,39 @@ public class MENetworkCover extends MECover implements IUICover {
         return MANAGED_FIELD_HOLDER;
     }
 
+    private void creteWidgetGroup() {
+        tabBar = new TabsWidget((index, widget) -> {
+            this.tabBarIndex = index;
+            this.currentPage = widget;
+            this.markAsDirty();
+            this.createUIWidget();
+        }, coverSlotHandler.length, 1, 40, 146, 10);
+    }
+
+    protected Widget currentPage;
+    protected WidgetGroup mainPage = new WidgetGroup(new Position(0, 0), new Size(146, 130));
+
     @Override
     public Widget createUIWidget() {
-        WidgetGroup group = new WidgetGroup(new Position(0, 0));
+        mainPage.clearAllWidgets();
+        mainPage.addWidget(tabBar);
         // ME Network status
-        group.addWidget(new LabelWidget(3, 5, () -> isOnline() ?
+        mainPage.addWidget(new LabelWidget(3, 5, () -> isOnline() ?
                 "gtceu.gui.me_network.online" :
                 "gtceu.gui.me_network.offline"));
 
         for (int i = 0; i < coverSlotHandler.length; i++) {
             CoverSlotHandler handler = coverSlotHandler[i];
-            group.addWidget(handler.createCoverSlotUI((i * 18), 20));
-            group.addWidget(handler.createFilterConfigUI(0, 34, 0, 96));
+            mainPage.addWidget(handler.createCoverSlotUI((i * 18) + 1, 20));
+            if (!handler.getSlotItem().isEmpty()) {
+                this.createSlotConfigUi(i);
+            }
         }
 
-        buildAdditionalUI(group);
+        mainPage.addWidget(currentPage);
+        buildAdditionalUI(mainPage);
 
-        return group;
+        return mainPage;
     }
 
 
@@ -98,13 +119,20 @@ public class MENetworkCover extends MECover implements IUICover {
         // Do nothing in the base implementation. This is intended to be overridden by subclasses.
     }
 
-    protected void configureSlot(CoverSlot slot, int type) {
+    protected void configureSlot(CoverSlot slot, int type, int index) {
         switch (type) {
             case CoverSlotHandler.LOADED_ID:
+                // 首次加载时会每个插槽都执行一次，不初始化ui
+                this.createSlotConfigUi(index);
+                if (this.isLoad) {
+                    this.tabBarIndex = index;
+                    tabBar.attachSelectTab(tabBarIndex);
+                }
                 slot.onLoad();
                 break;
             case CoverSlotHandler.REMOVE_ID:
                 slot.onRemove(this.getMainNode());
+                tabBar.attachRemoveTab(index);
                 break;
             case CoverSlotHandler.UPDATE_ID:
                 slot.onUpdate();
@@ -112,11 +140,24 @@ public class MENetworkCover extends MECover implements IUICover {
         }
     }
 
+    protected void createSlotConfigUi(int index) {
+        if (tabBar.isEmpty(index)) {
+            CoverSlotHandler handler = coverSlotHandler[index];
+            if (handler.getSlotItem().isEmpty()) {
+                tabBar.attachSubTab(index, TabsWidget.emptyPage);
+                return;
+            }
+            WidgetGroup groupConfig = new WidgetGroup(new Position(0, 0));
+            groupConfig.addWidget(handler.createFilterConfigUI(1, 35, 0, 96));
+            tabBar.attachSubTab(index, groupConfig);
+        }
+    }
+
     @Override
     public List<ItemStack> getAdditionalDrops() {
         var list = super.getAdditionalDrops();
         for (CoverSlotHandler handler : coverSlotHandler) {
-            if(!handler.getSlotItem().isEmpty()) {
+            if (!handler.getSlotItem().isEmpty()) {
                 list.add(handler.getSlotItem());
             }
         }
@@ -126,7 +167,7 @@ public class MENetworkCover extends MECover implements IUICover {
 
     /////////////////////////////////////
     // *** CAPABILITY OVERRIDE ***//
-    /////////////////////////////////////
+    /// //////////////////////////////////
 
     private CoverableItemHandlerWrapper itemHandlerWrapper;
 
---
+++ src/main/java/com/liser/appmachine/api/gui/widget/TabsWidget.java
@@ -0,0 +1,207 @@
+package com.liser.appmachine.api.gui.widget;
+
+import com.lowdragmc.lowdraglib.gui.texture.IGuiTexture;
+import com.lowdragmc.lowdraglib.gui.texture.ResourceTexture;
+import com.lowdragmc.lowdraglib.gui.widget.Widget;
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.util.Mth;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public class TabsWidget extends Widget {
+
+    protected final BiConsumer<Integer, Widget> onTabClick;
+    protected List<Widget> subTabs;
+    public static Widget emptyPage = new Widget(0, 0, 0, 0);
+    @Setter
+    protected IGuiTexture tabTexture = new ResourceTexture("gtceu:textures/gui/tab/tabs_top.png").getSubTexture(1 / 3f,
+            0, 1 / 3f, 0.5f);
+    @Setter
+    protected IGuiTexture tabHoverTexture = new ResourceTexture("gtceu:textures/gui/tab/tabs_top.png")
+            .getSubTexture(1 / 3f, 0.5f, 1 / 3f, 0.5f);
+    @Setter
+    protected IGuiTexture tabPressedTexture = tabHoverTexture;
+    @Getter
+    protected int offset = 0;
+    /**
+     * (old tab, new tab)
+     */
+    @Setter
+    @Nullable
+    protected BiConsumer<Widget, Widget> onTabSwitch;
+
+    @Nullable
+    protected Widget selectedTab;
+
+    public TabsWidget(BiConsumer<Integer, Widget> onTabClick) {
+        this(onTabClick, 0, 0, -20, 200, 24);
+    }
+
+    public TabsWidget(BiConsumer<Integer, Widget> onTabClick, int size, int x, int y, int width, int height) {
+        super(x, y, width, height);
+        this.subTabs = new ArrayList<>(Collections.nCopies(size, emptyPage));
+        this.onTabClick = onTabClick;
+    }
+
+    public void clearSubTabs() {
+        this.subTabs = new ArrayList<>(Collections.nCopies(subTabs.size(), emptyPage));
+    }
+
+    public void attachSubTab(int index, Widget subTab) {
+        subTabs.set(index, subTab);
+    }
+
+    public void attachRemoveTab(int index) {
+        subTabs.set(index, emptyPage);
+        onTabClick.accept(index, selectedTab);
+    }
+
+    public void attachSelectTab(int index) {
+        Widget newTab = subTabs.get(index);
+        if (newTab == selectedTab) {
+            return;
+        }
+        this.selectedTab = newTab;
+        onTabClick.accept(index, selectedTab);
+    }
+
+    public Widget attachGetTab(int index) {
+        return subTabs.get(index);
+    }
+
+    public boolean isEmpty(int index) {
+        return subTabs.get(index) == emptyPage;
+    }
+
+    @Override
+    public void handleClientAction(int id, FriendlyByteBuf buffer) {
+        super.handleClientAction(id, buffer);
+        if (subTabs.isEmpty()) {
+            selectedTab = emptyPage;
+            return;
+        }
+        if (id == 0) {
+            var index = buffer.readVarInt();
+            var old = selectedTab;
+            if (index < 0) {
+                selectedTab = subTabs.get(0);
+            } else if (index < subTabs.size()) {
+                selectedTab = subTabs.get(index);
+            } else {
+                return;
+            }
+            if (onTabSwitch != null) {
+                onTabSwitch.accept(old, selectedTab);
+            }
+            onTabClick.accept(index, selectedTab);
+        }
+    }
+
+    public int getSubTabsWidth() {
+        return getSize().width - 8 - 24 - 4 - 16 - 8 - 16;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (isMouseOverElement(mouseX, mouseY)) {
+            var hoveredTab = getHoveredTab(mouseX, mouseY);
+            // click tab
+            if (hoveredTab != null && hoveredTab != selectedTab && hoveredTab != emptyPage) {
+                if (onTabSwitch != null) {
+                    onTabSwitch.accept(selectedTab, hoveredTab);
+                }
+                selectedTab = hoveredTab;
+                writeClientAction(0,
+                        buf -> buf.writeVarInt(subTabs.indexOf(selectedTab)));
+                onTabClick.accept(subTabs.indexOf(selectedTab), selectedTab);
+                playButtonClickSound();
+            }
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public boolean mouseWheelMove(double mouseX, double mouseY, double wheelDelta) {
+        var sx = getPosition().x + 8 + 18 + 4 + 16;
+        if (isMouseOver(sx, getPosition().y, getSubTabsWidth(), 10, mouseX, mouseY)) {
+            offset = Mth.clamp(offset + 5 * (wheelDelta > 0 ? -1 : 1), 0, subTabs.size() * 24 - getSubTabsWidth());
+        }
+        return super.mouseWheelMove(mouseX, mouseY, wheelDelta);
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void drawInBackground(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+        super.drawInBackground(graphics, mouseX, mouseY, partialTicks);
+        var position = getPosition();
+        var size = getSize();
+        var hoveredTab = getHoveredTab(mouseX, mouseY);
+        // render sub tabs
+        for (int i = 0; i < subTabs.size(); i++) {
+            if (subTabs.get(i) == emptyPage) {
+                continue;
+            }
+            drawTab(subTabs.get(i), graphics, mouseX, mouseY, position.x + 18 * i, position.y, 18, 10, hoveredTab);
+        }
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void drawInForeground(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+        var hoveredTab = getHoveredTab(mouseX, mouseY);
+        if (hoveredTab != null && gui != null && gui.getModularUIGui() != null) {
+//            gui.getModularUIGui().setHoverTooltip(hoveredTab.getTabTooltips(), ItemStack.EMPTY, null,
+//                    hoveredTab.getTabTooltipComponent());
+        }
+        super.drawInForeground(graphics, mouseX, mouseY, partialTicks);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Nullable
+    public Widget getHoveredTab(double mouseX, double mouseY) {
+        if (isMouseOverElement(mouseX, mouseY)) {
+            var position = getPosition();
+            var size = getSize();
+            // others
+            int i = ((int) mouseX - position.x) / 18;
+            if (i < subTabs.size()) {
+                return subTabs.get(i);
+            }
+        }
+        return null;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void drawTab(Widget tab, @NotNull GuiGraphics graphics, int mouseX, int mouseY, int x, int y,
+                        int width, int height, Widget hoveredTab) {
+        // render background
+        if (tab == selectedTab) {
+            tabPressedTexture.draw(graphics, mouseX, mouseY, x, y, width, height);
+        } else if (tab == hoveredTab) {
+            tabHoverTexture.draw(graphics, mouseX, mouseY, x, y, width, height);
+        } else {
+            tabTexture.draw(graphics, mouseX, mouseY, x, y, width, height);
+        }
+        // render icon
+//        tab.getTabIcon().draw(graphics, mouseX, mouseY, x + (width - 16) / 2f, y + (height - 16) / 2f, 16, 16);
+    }
+
+    public void selectTab(Widget selectedTab) {
+        this.selectedTab = selectedTab;
+        this.detectAndSendChanges();
+    }
+
+
+}
